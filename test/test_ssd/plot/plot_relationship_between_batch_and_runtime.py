5f6b4d9b11b88c29f1e82c57b19704314bf75623
sugartom <sugartom.hu@gmail.com>
2020-02-17 09:26:28 -0800

    code backup

--- test/test_ssd/plot/plot_relationship_between_batch_and_runtime.py
+++ test/test_ssd/plot/plot_relationship_between_batch_and_runtime.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 batch_size = [1, 2, 4, 8, 16, 32, 64, 128]
 runtime = [0.055765, 0.121874, 0.354351, 0.377689, 0.683596, 1.353627, 2.601987, 5.169096]
 
+throughput = []
+for i in range(len(batch_size)):
+  throughput.append(batch_size[i] / runtime[i])
+
 plt.plot(batch_size, runtime, '-*', label = 'ssd_resnet')
+# plt.plot(batch_size, throughput, '-*', label = 'ssd_resnet')
 # plt.xscale('log')
 plt.xlabel('batch size')
 plt.ylabel('runtime (sec)')
