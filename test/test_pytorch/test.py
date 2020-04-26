a887f275814539e44ee63a6b57d958471df6e61a
sugartom <sugartom.hu@gmail.com>
2020-04-26 14:57:33 -0700

    moved test_pytorch model and input to device GPU

--- test/test_pytorch/test.py
+++ test/test_pytorch/test.py
@@ -1,10 +1,14 @@
+import numpy as np
+
+import torch
 import torchvision
 from torchvision.models.detection import FasterRCNN
 from PIL import Image
 import torchvision.transforms as transforms
 import time
 
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True).to(device)
 
 model.eval()
 
@@ -12,18 +16,20 @@ img_path = '/home/yitao/Documents/fun-project/tensorflow-related/post-rim/test/t
 img = Image.open(img_path)
 img = img.resize((800, 800))
 transform = transforms.Compose([transforms.ToTensor()])
-img = transform(img)
+img = transform(img).to(device)
 
-run_time = 10
-batch_size = 4
-images = []
-for i in range(batch_size):
-  images.append(img)
+run_time = 5
+batch_array = [1, 2, 3, 4]
 
-for i in range(10):
-  start = time.time()
-  predictions = model(images)
-  end = time.time()
-  print("duration = %f" % (end - start))
+for batch_size in batch_array:
+  images = []
+  for i in range(batch_size):
+    images.append(img)
 
-# print(predictions)
+  for i in range(run_time):
+    start = time.time()
+    predictions = model(images)
+    end = time.time()
+    print("duration = %f for batch size of %d" % (end - start, batch_size))
+
+
